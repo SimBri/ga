69ad4d8721bf64ca363d969b3d9f3648a70c76fc
SimBri <simonbrinkmann@gmail.com>
2023-09-24 16:07:54 +0200

    Pi test

---
+++ pi/index.js
@@ -0,0 +1,39 @@
+const Gpio = require('onoff').Gpio;
+const button = new Gpio(24, 'in', 'rising', {debounceTimeout: 1000});
+
+Gpio.accessible ?  console.log("GPIO Present") : console.log("GPIO not present");
+
+const TIMEOUT_MS = 5000;
+const timeOut = setTimeout(timeOutError, TIMEOUT_MS);
+
+
+
+function timeOutError(){
+  console.error("Button not pressed before timeout");
+  process.exitCode = 1;
+  button.unexport();
+}
+
+if (Gpio.accessible)
+button.watch((err, value) => {
+  if (err) {
+    throw err;
+  }
+
+  clearTimeout(timeOut); 
+  button.unexport();
+  console.log("Button pressed");
+
+    
+});
+
+process.on('SIGINT', _ => {
+  led.unexport();
+  button.unexport();
+});
+
+exit => {
+  
+}
+
+console.log("Wating for button to be pressed ... ");
