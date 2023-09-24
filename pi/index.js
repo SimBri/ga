f035ef56531716477a24b23917948fd23e529ff5
SimBri <simonbrinkmann@gmail.com>
2023-09-24 16:58:15 +0200

    Fix stop interval

--- pi/index.js
+++ pi/index.js
@@ -6,17 +6,18 @@ Gpio.accessible ?  console.log("GPIO Present") : console.log("GPIO not present")
 const TIMEOUT_MS = 30000;
 const timeOut = setTimeout(timeOutError, TIMEOUT_MS);
 
+let counter = 0;
+const counterInterval = setInterval(() => {
+  console.log("Waiting... " + ++counter);
+}, 1000);
 
 
 function timeOutError(){
   console.error("Button not pressed before timeout");
   button.unexport();
+  clearInterval(counterInterval);
   process.exitCode = 1;
 }
-let counter = 0;
-setInterval(() => {
-  console.log("Waiting... " + ++counter);
-}, 1000);
 
 if (Gpio.accessible)
 button.watch((err, value) => {
@@ -25,6 +26,7 @@ button.watch((err, value) => {
   }
 
   clearTimeout(timeOut); 
+  clearInterval(counterInterval);
   button.unexport();
   console.log("Button pressed");
 
@@ -37,10 +39,6 @@ process.on('SIGINT', _ => {
   led.unexport();
   button.unexport();
 });
-
-exit => {
-  
-}
 
 console.log("Wating for button to be pressed ");
 console.log(`Timeoutset to: ${TIMEOUT_MS} milliseconds`);
