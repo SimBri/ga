bf157fe12c5e51d12f034da9fe2fcf5e6c234b2d
Simon Brinkmann <simon@datek.no>
2024-04-29 07:48:50 +0200

    detecth

---
+++ version/common.h
@@ -0,0 +1,14 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <zephyr/sys/printk.h>
+
+#define FIRMWARE_VERSION "0.2.32"
+//extern char name[];
+extern char imsi[];
+/**
+ * @brief Returns the low byte of the 16-bit value \c n as an \c uint8_t.
+ */
+#define LOW_BYTE(n) ((uint8_t)((n)&0xFF))
