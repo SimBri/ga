47b98ff736d032fa521921ee5063e7b2c8c257e6
SimBri <simon@datek.no>
2024-04-30 06:12:34 +0200

    ReleaseFlow

--- README.md
+++ README.md
@@ -3,4 +3,3 @@ Testing Github Actions
 
 Doing some change
 
-Doing some more changes
--- version/common.h
+++ version/common.h
@@ -5,7 +5,7 @@
 #include <stdint.h>
 #include <zephyr/sys/printk.h>
 
-#define FIRMWARE_VERSION "0.2.36"
+#define FIRMWARE_VERSION "0.2.37"
 //extern char name[];
 extern char imsi[];
 /**
